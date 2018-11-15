3939ed4ef3d187e5c9222482cdbf5926ebb16da0
angeljiang0927@gmail.com <angeljiang0927@gmail.com>
2018-11-14 22:51:52 -0800

    finished veiws, added public

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var PORT = process.env.PORT || 8080;
 
 var app = express();
 
-app.use(express.static("public"));
+// app.use(express.static("public"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,9 +14,9 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var routes = require("");
+var router = require("./controllers/burgers_controllers");
 
-app.use(routes);
+app.use(router);
 
 app.listen(PORT, function() {
     console.log("Server listening on: http://localhost:" + PORT);
