43d2218a3833844ef6b76035c92d323a1b4d17d1
angeljiang0927@gmail.com <angeljiang0927@gmail.com>
2018-11-16 00:40:09 -0800

    changed all route to index

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -2,15 +2,6 @@ var express = require("express");
 var router = express.Router();
 var burger = require("../models/burger.js");
 
-router.get('/', function(req, res) {
-    burger.all(function(data) {
-        var hbsObject = {
-            burger: data
-        };
-        console.log(hbsObject);
-        res.render("index", hbsObject);
-    });
-});
 
 router.post('/api/burgers', function(req, res) {
     console.log(req.body.name + " and " + req.body.devoured);
@@ -37,6 +28,17 @@ router.put("/api/burgers/:id", function(req, res) {
         else {
             res.status(200).end();
         }
+    });
+});
+
+
+router.get('*', function(req, res) {
+    burger.all(function(data) {
+        var hbsObject = {
+            burger: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
     });
 });
 
